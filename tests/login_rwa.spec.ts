723211bd0be7f367e3fa60f664a21c9b99fee9a1
pandapo777 <85247214+pandapo777@users.noreply.github.com>
2025-09-28 20:11:58 +0300

    homework locators

---
+++ tests/login_rwa.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://rwa-194.87.102.103.sslip.io/login');
+
+  await expect(page).toHaveTitle(/Login | next.js RealWorld example app/);
+
+
+});
+
+test('has field "password"', async ({ page }) => {
+  await page.goto('https://rwa-194.87.102.103.sslip.io/login');
+
+  await expect(page.getByTestId('input-password')).toBeVisible();
+
+});
+
+test('has "btn-submit"', async ({ page }) => {
+  await page.goto('https://rwa-194.87.102.103.sslip.io/login');
+
+  await expect(page.getByTestId('btn-submit')).toBeVisible();
+
+});
+test('get link "Sign in"', async ({ page }) => {
+  await page.goto('https://rwa-194.87.102.103.sslip.io/');
+
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
+
+});
