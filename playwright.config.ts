a23d7c737ffde35f126b5c08582fa7aa621603f2
pandapo777 <85247214+pandapo777@users.noreply.github.com>
2025-12-06 17:17:01 +0300

    изменения в конфиге playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,13 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  // Глобальное выполнение теста
+  //Нужно чтобы избежать бесконечных ожиданий, но при этом учитывать медленные операции
+  timeout: 30000,
+  //Настроим timeout для expect, чтобы тесты не падали слишком рано, если элемент загружается немного дольше.
+  expect: {timeout: 5000},
   /* Fail the build on CI if you accidentally left test.only in the source code. */
+  //Если тесты запускаются в CI (то есть переменная CI установлена), то forbidOnly становится true.
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
@@ -27,9 +33,18 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
+    //Настраиваем дата-атрибут
+    testIdAttribute: "id",
+    //  testIdAttribute: "testId",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // Делаем скрин или трейс при падении тестов
+    trace: 'retain-on-failure',
+    // trace: 'on-first-retry',
+    //Режим запуска браузера без графического интерфейса, для CI обычно true, для локальной отладки - false
+    headless: false,
+    //viewport: Размеры дного браузера (например, { width : 1280, height : 720 })
+    // screenshot, video: Опции для автоматической записи скриншотов/видео в случае сбоев, что полезно для отладки.
   },
 
   /* Configure projects for major browsers */
@@ -39,15 +54,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
